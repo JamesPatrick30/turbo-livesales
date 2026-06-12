98a28a251e7373c743588eb1634b3041788b34ae
Jamespatrick30 <mrsniper1388@gmail.com>
2026-06-12 20:49:17 +0800

    chore: remove the unused page and import the new page

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,14 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "../pages/Home";
-import About from "../pages/About";
+import LandingPage from "../pages/LandingPage";
+import Demo from "../pages/Demo";
 
+// Admin Pages
+import AdminOrders from "../pages/AdminPages/AdminOrders";
+import AdminMenu from "../pages/AdminPages/AdminMenu";
+import AdminAccounts from "../pages/AdminPages/AdminAccounts";
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/demo" element={<Demo />} />
+        <Route path="/demo/admin" element={<AdminOrders />} />
+        <Route path="/demo/admin/orders" element={<AdminOrders />} />
+        <Route path="/demo/admin/menu" element={<AdminMenu />} />
+        <Route path="/demo/admin/accounts" element={<AdminAccounts />} />
+        {/* Add more routes as needed */}
       </Routes>
     </BrowserRouter>
   );
