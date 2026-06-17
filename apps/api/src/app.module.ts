8f5adc79fd16233e191c6c9677845851749d4a52
Jamespatrick30 <mrsniper1388@gmail.com>
2026-06-17 19:56:51 +0800

    chore: import users

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,8 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true })], // Load .env and make it globally available
+  imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true }), PrismaModule, UsersModule], // Load .env and make it globally available
   controllers: [AppController],
   providers: [AppService],
 })
